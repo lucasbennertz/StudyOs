f8f928cf4d12d126c78b802f9e26f26972c80d61
lucasbennertz <lucasgcbennertz@gmail.com>
2025-05-20 21:23:10 -0300

    ligação apis

--- src/app/views/materias/materias.service.ts
+++ src/app/views/materias/materias.service.ts
@@ -5,35 +5,58 @@ import { MatterModel } from './materia.model';
 import { AnnotationModel } from './annotation.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MateriasService {
-  annotationUrl: string = "http://localhost:8080/annotations"
-  mattersUrl: string = "http://localhost:8080/matters";
+  annotationUrl: string =
+    'https://api-annotationsbymatters.railway.internal/annotations';
+  mattersUrl: string = 'https://api-annotationsbymatters.railway.internal/matters';
   constructor(private http: HttpClient) {}
 
-  readMatter(): Observable<MatterModel[]>{
+  readMatter(): Observable<MatterModel[]> {
     return this.http.get<MatterModel[]>(this.mattersUrl);
   }
-  changeMatterName(matterId : string, newMatterName: string): Observable<MatterModel>{
-    return this.http.put<MatterModel>(this.mattersUrl + "/" + matterId, {"nome" : newMatterName});
+  changeMatterName(
+    matterId: string,
+    newMatterName: string
+  ): Observable<MatterModel> {
+    return this.http.put<MatterModel>(this.mattersUrl + '/' + matterId, {
+      nome: newMatterName,
+    });
   }
-  deleteMatter(matterId : string): Observable<MatterModel[]>{
-    return this.http.delete<MatterModel[]>(this.mattersUrl + "/" + matterId)
+  deleteMatter(matterId: string): Observable<MatterModel[]> {
+    return this.http.delete<MatterModel[]>(this.mattersUrl + '/' + matterId);
   }
-  createMatter(matterName: String): Observable<MatterModel>{
-    return this.http.post<MatterModel>(this.mattersUrl, {"nome" : matterName})
+  createMatter(matterName: String): Observable<MatterModel> {
+    return this.http.post<MatterModel>(this.mattersUrl, { nome: matterName });
   }
-  getAnnotations(matterId: string): Observable<AnnotationModel[]>{
-    return this.http.get<AnnotationModel[]>(this.mattersUrl + "/" + matterId + "/" + "annotations");
+  getAnnotations(matterId: string): Observable<AnnotationModel[]> {
+    return this.http.get<AnnotationModel[]>(
+      this.mattersUrl + '/' + matterId + '/' + 'annotations'
+    );
   }
-  changeAnnotationName(annotationId: string, newAnnotationName: string): Observable<AnnotationModel>{
-    return this.http.put<AnnotationModel>(this.annotationUrl + "/" + annotationId + "/" + "rename", {"title" : newAnnotationName});
+  changeAnnotationName(
+    annotationId: string,
+    newAnnotationName: string
+  ): Observable<AnnotationModel> {
+    return this.http.put<AnnotationModel>(
+      this.annotationUrl + '/' + annotationId + '/' + 'rename',
+      { title: newAnnotationName }
+    );
   }
-  createAnnotation(matterId: string, annotationName: string, annotationDescription: string): Observable<AnnotationModel>{
-    return this.http.post<AnnotationModel>(this.annotationUrl + "/" + matterId, ({"title" : annotationName, "description" : annotationDescription }));
+  createAnnotation(
+    matterId: string,
+    annotationName: string,
+    annotationDescription: string
+  ): Observable<AnnotationModel> {
+    return this.http.post<AnnotationModel>(
+      this.annotationUrl + '/' + matterId,
+      { title: annotationName, description: annotationDescription }
+    );
   }
-  deleteAnnotation(matterId: string): Observable<AnnotationModel[]>{
-    return this.http.delete<AnnotationModel[]>(this.annotationUrl + "/" + matterId);
+  deleteAnnotation(matterId: string): Observable<AnnotationModel[]> {
+    return this.http.delete<AnnotationModel[]>(
+      this.annotationUrl + '/' + matterId
+    );
   }
 }
