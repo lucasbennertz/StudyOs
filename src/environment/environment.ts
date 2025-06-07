44bcf8b4ed180fdaf5723315de43c33dcdbf4d65
lucasbennertz <lucasgcbennertz@gmail.com>
2025-06-07 11:34:34 -0300

    alinhado com o azure e nginx

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
-    annotationURL: 'http://172.210.176.252:8080/annotations',
-    matterURL: 'http://172.210.176.252:8080/matters'
-}
+  production: false,
+  annotationURL: 'https://testestudyos.eastus2.cloudapp.azure.com/annotations',
+  matterURL: 'https://testestudyos.eastus2.cloudapp.azure.com/matters',
+};
